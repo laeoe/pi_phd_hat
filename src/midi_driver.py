8632d934e583529d61211efa12ba5969e4a025ab
laeoe <laeoes.spam@gmail.com>
2023-11-30 18:39:17 +0000

    first test of reading midi messages by pi

---
+++ src/midi_driver.py
@@ -0,0 +1,19 @@
+import mido
+
+# List available MIDI input ports (find your MIDI keyboard)
+input_ports = mido.get_input_names()
+
+if not input_ports:
+    print("No MIDI input ports found. Make sure your MIDI keyboard is connected.")
+else:
+    # Open the first available MIDI input port
+    with mido.open_input(input_ports[1]) as port:
+        print(f"Listening to MIDI input from {input_ports[0]}...")
+        
+        try:
+            # Infinite loop to continuously receive MIDI messages
+            for message in port:
+                print(f"Received MIDI message: {message}")
+        
+        except KeyboardInterrupt:
+            print("\nKeyboard Interrupt, Exiting...")
