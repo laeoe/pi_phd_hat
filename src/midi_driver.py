bcbd88a2e847ccf1750a8353d94c39dc093ce08e
laeoe <laeoes.spam@gmail.com>
2023-12-06 16:46:44 +0100

    adapted for akai keyboard, readout with macbook

--- src/midi_driver.py
+++ src/midi_driver.py
@@ -2,12 +2,13 @@ import mido
 
 # List available MIDI input ports (find your MIDI keyboard)
 input_ports = mido.get_input_names()
+print(input_ports)
 
 if not input_ports:
     print("No MIDI input ports found. Make sure your MIDI keyboard is connected.")
 else:
     # Open the first available MIDI input port
-    with mido.open_input(input_ports[1]) as port:
+    with mido.open_input(input_ports[0]) as port:
         print(f"Listening to MIDI input from {input_ports[0]}...")
         
         try:
