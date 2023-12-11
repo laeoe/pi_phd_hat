8ecdba60bfb20e8255f60ad91c5dff5887872ae3
laeoe <laeoes.spam@gmail.com>
2023-12-11 13:35:06 +0100

    Refactor key_freq function and add multiple oscillators for simultaneous notes notes

--- src/test.py
+++ src/test.py
@@ -13,9 +13,9 @@ osc.stop()
 
 
 # calculate the frequency of a piano key
-def key_freq(key):
-    a = 440 # Base tone at 440Hz
-    return a * 2 **((key - 57) / 12) # currentely the number of a4 is wrong!
+def key_2freq(key):
+    a = 440 # Base tone a4 = 440Hz
+    return a * 2 **((key - 57) / 12)
 
 
 # Find and open the MIDI Keyboard
@@ -28,18 +28,27 @@ for input_name in mido.get_input_names():
 
 if not midi_input:
     raise Exception("MIDI Keyboard not found.")
+    
+
+# Create the oscillators for the Keyboard
+oscillators = {}
+for note in range(128):  # MIDI has 128 notes
+    oscillators[note] = Sine(freq=key_2freq(note))
 
 # Function to process MIDI messages
 def process_midi_message(msg):
     if msg.type == 'note_on' and msg.velocity > 0:
-        # Start the oscillator with the frequency of the note
-        # osc.freq = midi_to_freq(msg.note)
-        osc.freq = key_freq(msg.note)
-        osc.out()
-        print(f"freq = {key_freq(msg.note)}")
+        note = msg.note
+        velocity = msg.velocity
+        # Scale the velocity to an appropriate multiplier value (0-1)
+        amplitude = velocity / 127
+        oscillators[note].mul = amplitude
+        oscillators[note].freq = key_2freq(note)
+        oscillators[note].out()
     elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
-        # Stop the oscillator
-        osc.stop()
+        note = msg.note
+        oscillators[note].stop()
+
 
 # Main loop to read MIDI messages
 try:
