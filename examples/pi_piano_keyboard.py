bb8f840c5b74aca3907ccf000d6cb7d605dd45ea
laeoe <laeoes.spam@gmail.com>
2023-12-12 11:55:02 +0100

    added the key samples to the repo

---
+++ examples/pi_piano_keyboard.py
@@ -0,0 +1,6 @@
+from pi_phd_hat.midi_driver import MidiDriver
+from pi_phd_hat.audio_driver import AudioPlayer
+
+
+# Initialize and preload audio data
+samples_path = "/home/pi/pi_phd_hat/assets/key_samples"
