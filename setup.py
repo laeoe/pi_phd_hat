d9e5674ba15927847bc0390842aa2d33de674824
laeoe <laeoes.spam@gmail.com>
2023-12-12 15:59:36 +0100

    updated requirements to be included on install

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='pi_phd_hat',
     version='0.1.0',
@@ -8,14 +11,7 @@ setup(
     package_data={
         'pi_phd_hat': ['assets/key_samples/*']
     },
-    install_requires=[
-        # 'mido', 
-        # 'numpy',
-        # 'pyaudio',
-        # 'wave',
-        # 'concurrent',
-        # 'scipy',  
-    ],
+    install_requires=required,
     # entry_points={
     #     'console_scripts': [
     #         'my_midi_project=my_midi_project.__main__:main',  # If you have a main script
@@ -23,7 +19,7 @@ setup(
     # },
     # Additional metadata
     author='Leo Walz',
-    author_email='your.email@example.com',
+    author_email='laeoes.spam@gmail.com',
     description='Phd hat project that includes a midi keyboard and a rasberry pi',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
